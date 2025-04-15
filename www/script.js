44463f888fcde3cb537eafc3b969d292381b61a7
THEUZSN <ggmat304@gmail.com>
2025-04-16 02:09:54 +0900

    correção Number

--- www/script.js
+++ www/script.js
@@ -5,5 +5,5 @@ document.addEventListener("deviceready", onDeviceReady, false );
 
 function vibrar(){
     navigator.vibrate(Number(document.getElementById("tempo").value)*1000);
-    alert(`vibrou por ${umber(document.getElementById("tempo").value)*1000}ms`)
+    alert(`vibrou por ${Number(document.getElementById("tempo").value)*1000}ms`)
 }
